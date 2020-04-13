36e7ed4bdad07349bd6e8a719eb51a573175e520
camelya58 <camelya@mail.ru>
2020-04-13 20:38:33 +0300

    add simple project with spring boot and jsp

---
+++ src/main/java/com/github/camelya58/controllers/HomeController.java
@@ -0,0 +1,39 @@
+package com.github.camelya58.controllers;
+
+
+import com.github.camelya58.models.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+
+@Controller
+public class HomeController {
+
+    private ArrayList<Book> books = new ArrayList<>();
+
+    @GetMapping({"/", "/home"})
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model, @RequestParam String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/books")
+    public String addBook(Model model, @RequestParam(required = false) String name) {
+        if (name != null) books.add(new Book(name));
+        model.addAttribute("books", books);
+        return "books";
+    }
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/views/books.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<form action="/books">
+    <input type="text" name="name" />
+    <input type="submit">
+</form>
+
+<c:forEach items="${books}" var="book">
+    <span>${book.name}</span><br>
+</c:forEach>
